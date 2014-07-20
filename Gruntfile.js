4af951dfcf3b179b068c7c01a941cf17b2d2ab9b
Benjamin Becquet <benjamin.becquet@gmail.com>
2014-07-20 23:04:29 +0200

    Add a watcher to the Grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,15 @@ module.exports = function(grunt) {
         src: ['src/jKstra.js', 'src/core/*.js', 'src/algos/*.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
+    },
+    watch: {
+      files: ['src/**/*.js'],
+      tasks: ['concat']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['concat']);
 }
