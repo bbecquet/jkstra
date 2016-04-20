bb8e8557d0263fc78b8f114e17bbc69e49482caa
bbecquet <benjamin.becquet@gmail.com>
2016-04-20 15:17:16 +0200

    ES6: Graph as class

--- dist/core/Graph.js
+++ dist/core/Graph.js
@@ -4,25 +4,36 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _constants = require('./constants.js');
 
 var _utils = require('./utils.js');
 
-var Graph = function Graph() {
-    var vertices = [];
-    var edges = [];
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-    return {
-        addVertex: function addVertex(data) {
+var Graph = function () {
+    function Graph() {
+        _classCallCheck(this, Graph);
+
+        this.vertices = [];
+        this.edges = [];
+    }
+
+    _createClass(Graph, [{
+        key: 'addVertex',
+        value: function addVertex(data) {
             var vertex = {
                 _in: [],
                 _out: [],
                 data: data
             };
-            vertices.push(vertex);
+            this.vertices.push(vertex);
             return vertex;
-        },
-        addEdge: function addEdge(from, to, data) {
+        }
+    }, {
+        key: 'addEdge',
+        value: function addEdge(from, to, data) {
             var edge = {
                 from: from,
                 to: to,
@@ -30,95 +41,121 @@ var Graph = function Graph() {
             };
             from._out.push(edge);
             to._in.push(edge);
-            edges.push(edge);
+            this.edges.push(edge);
             return edge;
-        },
-
+        }
 
         /**
         Shortcut to add an edge and its reverse,
         sharing the same data.
         */
-        addEdgePair: function addEdgePair(a, b, data) {
+
+    }, {
+        key: 'addEdgePair',
+        value: function addEdgePair(a, b, data) {
             return [this.addEdge(a, b, data), this.addEdge(b, a, data)];
-        },
-        removeEdge: function removeEdge(edge) {
-            var index = edges.indexOf(edge);
+        }
+    }, {
+        key: 'removeEdge',
+        value: function removeEdge(edge) {
+            var index = this.edges.indexOf(edge);
             if (index !== -1) {
-                // remove from extremity vertices first
+                // remove from extremity this.vertices first
                 edge.from._out.splice(edge.from._out.indexOf(edge), 1);
                 edge.to._in.splice(edge.to._in.indexOf(edge), 1);
-                edges.splice(index, 1);
+                this.edges.splice(index, 1);
             }
-        },
-        removeVertex: function removeVertex(vertex) {
-            var index = vertices.indexOf(vertex);
+        }
+    }, {
+        key: 'removeVertex',
+        value: function removeVertex(vertex) {
+            var index = this.vertices.indexOf(vertex);
             if (index !== -1) {
-                // remove all incident edges first
+                // remove all incident this.edges first
                 var edgesToRemove = vertex._in.concat(vertex._out);
                 for (var i = 0; i < edgesToRemove.length; i++) {
                     this.removeEdge(edgesToRemove[i]);
                 }
-                vertices.splice(index, 1);
+                this.vertices.splice(index, 1);
             }
-        },
-        outEdges: function outEdges(vertex, filter) {
+        }
+    }, {
+        key: 'outEdges',
+        value: function outEdges(vertex, filter) {
             return this.incidentEdges(vertex, _constants.OUT, filter);
-        },
-        inEdges: function inEdges(vertex, filter) {
+        }
+    }, {
+        key: 'inEdges',
+        value: function inEdges(vertex, filter) {
             return this.incidentEdges(vertex, _constants.IN, filter);
-        },
-
+        }
 
         /**
-        Returns all edges incident to a vertex, in one direction (outgoing or incoming),
+        Returns all this.edges incident to a vertex, in one direction (outgoing or incoming),
         optionnaly filtered by a given function.
         */
-        incidentEdges: function incidentEdges(vertex, direction, filter) {
+
+    }, {
+        key: 'incidentEdges',
+        value: function incidentEdges(vertex, direction, filter) {
             if (!filter) {
                 return direction ? vertex._out : vertex._in;
             }
             var edges = direction ? vertex._out : vertex._in;
             return edges.filter(filter);
-        },
-        vertex: function vertex(props) {
+        }
+    }, {
+        key: 'vertex',
+        value: function vertex(props) {
+            var vertices = this.vertices;
             for (var i = 0, l = vertices.length; i < l; i++) {
                 if ((0, _utils.propsMatch)(vertices[i].data, props)) {
                     return vertices[i];
                 }
             }
             return null;
-        },
-        edge: function edge(props) {
+        }
+    }, {
+        key: 'edge',
+        value: function edge(props) {
+            var edges = this.edges;
             for (var i = 0, l = edges.length; i < l; i++) {
                 if ((0, _utils.propsMatch)(edges[i].data, props)) {
                     return edges[i];
                 }
             }
             return null;
-        },
-
+        }
 
         /**
         Perform an action on each vertex of the graph
         */
-        forEachVertex: function forEachVertex(action) {
-            vertices.forEach(function (v) {
+
+    }, {
+        key: 'forEachVertex',
+        value: function forEachVertex(action) {
+            this.vertices.forEach(function (v) {
                 return action(v);
             });
-        },
-
+        }
 
         /**
         Perform an action on each edge of the graph
         */
-        forEachEdge: function forEachEdge(action) {
-            edges.forEach(function (e) {
+
+    }, {
+        key: 'forEachEdge',
+        value: function forEachEdge(action) {
+            this.edges.forEach(function (e) {
                 return action(e);
             });
         }
-    };
-};
+    }]);
+
+    return Graph;
+}();
+
+;
 
 exports.default = Graph;
 module.exports = exports['default'];
--- src/core/Graph.js
+++ src/core/Graph.js
@@ -1,118 +1,120 @@
 import { IN, OUT } from './constants.js';
 import { propsMatch } from './utils.js';
 
-const Graph = function() {
-    const vertices = [];
-    const edges = [];
+class Graph {
+    constructor() {
+        this.vertices = [];
+        this.edges = [];
+    }
 
-    return {
-        addVertex(data) {
-            const vertex = {
-                _in: [],
-                _out: [],
-                data: data
-            };
-            vertices.push(vertex);
-            return vertex;
-        },
+    addVertex(data) {
+        const vertex = {
+            _in: [],
+            _out: [],
+            data: data
+        };
+        this.vertices.push(vertex);
+        return vertex;
+    }
 
-        addEdge(from, to, data) {
-            const edge = {
-                from: from,
-                to: to,
-                data: data || {}
-            };
-            from._out.push(edge);
-            to._in.push(edge);
-            edges.push(edge);
-            return edge;
-        },
+    addEdge(from, to, data) {
+        const edge = {
+            from: from,
+            to: to,
+            data: data || {}
+        };
+        from._out.push(edge);
+        to._in.push(edge);
+        this.edges.push(edge);
+        return edge;
+    }
 
-        /**
-        Shortcut to add an edge and its reverse,
-        sharing the same data.
-        */
-        addEdgePair(a, b, data) {
-            return [
-                this.addEdge(a, b, data),
-                this.addEdge(b, a, data)
-            ];
-        },
+    /**
+    Shortcut to add an edge and its reverse,
+    sharing the same data.
+    */
+    addEdgePair(a, b, data) {
+        return [
+            this.addEdge(a, b, data),
+            this.addEdge(b, a, data)
+        ];
+    }
 
-        removeEdge(edge) {
-            const index = edges.indexOf(edge);
-            if(index !== -1) {
-                // remove from extremity vertices first
-                edge.from._out.splice(edge.from._out.indexOf(edge), 1);
-                edge.to._in.splice(edge.to._in.indexOf(edge), 1);
-                edges.splice(index, 1);
-            }
-        },
-
-        removeVertex(vertex) {
-            const index = vertices.indexOf(vertex);
-            if(index !== -1) {
-                // remove all incident edges first
-                const edgesToRemove = vertex._in.concat(vertex._out);
-                for(let i = 0; i < edgesToRemove.length; i++) {
-                    this.removeEdge(edgesToRemove[i]);
-                }
-                vertices.splice(index, 1);
-            }
-        },
-
-        outEdges(vertex, filter) {
-            return this.incidentEdges(vertex, OUT, filter);
-        },
-
-        inEdges(vertex, filter) {
-            return this.incidentEdges(vertex, IN, filter);
-        },
-
-        /**
-        Returns all edges incident to a vertex, in one direction (outgoing or incoming),
-        optionnaly filtered by a given function.
-        */
-        incidentEdges(vertex, direction, filter) {
-            if(!filter) {
-                return direction ? vertex._out : vertex._in;
-            }
-            const edges = direction ? vertex._out : vertex._in;
-            return edges.filter(filter);
-        },
-
-        vertex(props) {
-            for(let i = 0, l = vertices.length; i < l; i++) {
-                if(propsMatch(vertices[i].data, props)) {
-                    return vertices[i];
-                }
-            }
-            return null;
-        },
-
-        edge(props) {
-            for(let i = 0, l = edges.length; i < l; i++) {
-                if(propsMatch(edges[i].data, props)) {
-                    return edges[i];
-                }
-            }
-            return null;
-        },
-
-        /**
-        Perform an action on each vertex of the graph
-        */
-        forEachVertex(action) {
-            vertices.forEach(v => action(v));
-        },
-
-        /**
-        Perform an action on each edge of the graph
-        */
-        forEachEdge(action) {
-            edges.forEach(e => action(e));
+    removeEdge(edge) {
+        const index = this.edges.indexOf(edge);
+        if(index !== -1) {
+            // remove from extremity this.vertices first
+            edge.from._out.splice(edge.from._out.indexOf(edge), 1);
+            edge.to._in.splice(edge.to._in.indexOf(edge), 1);
+            this.edges.splice(index, 1);
         }
-    };
+    }
+
+    removeVertex(vertex) {
+        const index = this.vertices.indexOf(vertex);
+        if(index !== -1) {
+            // remove all incident this.edges first
+            const edgesToRemove = vertex._in.concat(vertex._out);
+            for(let i = 0; i < edgesToRemove.length; i++) {
+                this.removeEdge(edgesToRemove[i]);
+            }
+            this.vertices.splice(index, 1);
+        }
+    }
+
+    outEdges(vertex, filter) {
+        return this.incidentEdges(vertex, OUT, filter);
+    }
+
+    inEdges(vertex, filter) {
+        return this.incidentEdges(vertex, IN, filter);
+    }
+
+    /**
+    Returns all this.edges incident to a vertex, in one direction (outgoing or incoming),
+    optionnaly filtered by a given function.
+    */
+    incidentEdges(vertex, direction, filter) {
+        if (!filter) {
+            return direction ? vertex._out : vertex._in;
+        }
+        const edges = direction ? vertex._out : vertex._in;
+        return edges.filter(filter);
+    }
+
+    vertex(props) {
+        const vertices = this.vertices;
+        for (let i = 0, l = vertices.length; i < l; i++) {
+            if (propsMatch(vertices[i].data, props)) {
+                return vertices[i];
+            }
+        }
+        return null;
+    }
+
+    edge(props) {
+        const edges = this.edges;
+        for (let i = 0, l = edges.length; i < l; i++) {
+            if (propsMatch(edges[i].data, props)) {
+                return edges[i];
+            }
+        }
+        return null;
+    }
+
+    /**
+    Perform an action on each vertex of the graph
+    */
+    forEachVertex(action) {
+        this.vertices.forEach(v => action(v));
+    }
+
+    /**
+    Perform an action on each edge of the graph
+    */
+    forEachEdge(action) {
+        this.edges.forEach(e => action(e));
+    }
 };
 
 export default Graph;
